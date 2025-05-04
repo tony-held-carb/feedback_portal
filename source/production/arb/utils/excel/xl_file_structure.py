d239e152824083e46be0a563ac2cd4cd9c296401
Tony Held <tony.held@arb.ca.gov>
2025-05-04 09:16:30 -0700

    changing xl_create for new path structure

--- source/production/arb/utils/excel/xl_file_structure.py
+++ source/production/arb/utils/excel/xl_file_structure.py
@@ -7,10 +7,19 @@ from arb.utils.file_io import get_project_root_dir
 
 logger, pp_log = get_logger.get_logger(__name__, __file__)
 
-# Get the platform independent project root directory knowing the utils directory structure is:
-# 'feedback_portal/source/production/arb/utils/'
-app_dir_structure = ['feedback_portal', 'source', 'production', 'arb', 'utils']
-PROJECT_ROOT = get_project_root_dir(__file__, app_dir_structure)
+# Get the platform independent project root of the feedback portal
+# The utility model may be initialized by running the module directly,
+# or it may be initialized by a flask app.  Try to get the 'feedback_portal' either way
+
+try:
+  app_dir_structure = ['feedback_portal', 'source', 'production', 'arb', 'utils']
+  PROJECT_ROOT = get_project_root_dir(__file__, app_dir_structure)
+  logger.debug(f"{PROJECT_ROOT =}, determined by {app_dir_structure =}")
+except ValueError as e:
+  app_dir_structure = ['feedback_portal', 'source', 'production', 'arb', 'portal']
+  PROJECT_ROOT = get_project_root_dir(__file__, app_dir_structure)
+  logger.debug(f"{PROJECT_ROOT =}, determined by {app_dir_structure =}")
+
 FEEDBACK_FORMS = PROJECT_ROOT / "feedback_forms"
 CURRENT_VERSIONS = FEEDBACK_FORMS / "current_versions"
 PROCESSED_VERSIONS = FEEDBACK_FORMS / "processed_versions"
