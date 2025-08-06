fe2e386ab54d4f74f2a997c8ed85b71a030b8056
tony-held-carb <tony.held@arb.ca.gov>
2025-08-06 16:44:45 -0700

    going to convert work to lf

---
+++ debugging/convert_work_to_lf.sh
@@ -0,0 +1,1 @@
+find . -type f -not -path "./.git/*" -not -path "./.idea/*" \( -name "*.py" -o -name "*.txt" -o -name "*.md" -o -name "*.json" -o -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.sh" -o -name "*.yml" -o -name "*.yaml" -o -name "*.xml" -o -name "*.sql" -o -name "*.ini" -o -name "*.cfg" -o -name "*.conf" \) -exec dos2unix {} \;
